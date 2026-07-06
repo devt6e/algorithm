9c7ff93fe0ad5d47ee4c0c2025acad5bdefa3675
TaeHyeong <devt6e@gmail.com>
2026-07-06 09:19:08 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.01 ms, Memory: 4.64 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.cpp
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.cpp
@@ -6,10 +6,8 @@ using namespace std;
 vector<int> solution(long long n) {
     vector<int> answer;
     int item = 0;
-    while(1)
+    while(n)
     {
-        if(n <= 0)
-            break;
         item = n%10;
         answer.push_back(item);
         n /= 10;
